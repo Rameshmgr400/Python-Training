b170339416af0ab80488366d212e406fa6d55bfc
Rameshmgr400 <rameshthapamgr400@gmail.com>
2021-12-13 09:44:48 +0545

    First Commint

--- rollingdice.py
+++ rollingdice.py
@@ -2,7 +2,7 @@ import random
 min_num = 1
 max_num = 8
 roll =  "yes"
-while roll == "Yes" or roll == "y"
+while roll == "Yes" or roll == "y":
     print("Keep Rolling...")
     print("The Values Are :")
     print(random.randint(min_num, max_num))
@@ -13,5 +13,5 @@ while roll == "Yes" or roll == "y"
     print(r)
     print(r1)
     roll - input("Can i roll again the dice? (yes/no) : ")
-    if roll == "no" or roll == "n"
-    print("Thank for playing, can play again!")
+    if roll == "no" or roll == "n":
+        print("Thank for playing, can play again!")
